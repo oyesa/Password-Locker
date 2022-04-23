81ca2640a4bf10a2d00731da514b79d6b9498c3f
oyesa <mercyoyesa2018@gmail.com>
2022-04-23 10:31:44 +0300

    create method to save and delete user in user-list

--- user.py
+++ user.py
@@ -9,9 +9,28 @@ class User:
     user_list = []
 
     def __init__(self, username, password):
+
         """
-        method that defines the properties of a user.
+        instantiate user properties
         """
         self.username = username
         self.password = password
+
+    def save_user(self):
+
+        """
+        method to saves new user to user list
+        """
+        User.user_list.append(self)
+
+    @classmethod
+    def display_user(cls):
+        return cls.user_list
+
+    def delete_user(self):
+
+        """
+        method to delete a  saved account from the user_list
+        """
+        User.user_list.remove(self)    
 
