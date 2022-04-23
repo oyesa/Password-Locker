05d154053f44f8167947a8af8babddb48ae13455
oyesa <mercyoyesa2018@gmail.com>
2022-04-23 11:32:04 +0300

    add test to confirm whether user has been initialised

--- user-test.py
+++ user-test.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest    #import unittest module to construct and run tests
 from user import User  #import user class
 from user import Details #import details class
 
@@ -14,3 +14,11 @@ class TestClass(unittest.TestCase):
         First run method before other test methods
         """
         self.new_user = User('OyesaOluchina','NotI793fl0')
+
+    def test_init(self):
+
+        """
+        test if object has been initialized
+        """
+        self.assertEqual(self.new_user.username,'OyesaOluchina')
+        self.assertEqual(self.new_user.password,'NotI793fl0')
