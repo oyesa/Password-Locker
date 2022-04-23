3bc028d21806fc00594409cb089f2433d08db516
oyesa <mercyoyesa2018@gmail.com>
2022-04-23 13:15:49 +0300

    create function to display existing user if they exist

--- main.py
+++ main.py
@@ -11,8 +11,23 @@ def create_new_user(username,password):
     return new_user
 
 def save_user(user):
-  
+
     """
-    Function to save a new user
+    function to save new user
     """
     user.save_user()
+  
+def display_user():
+
+    """
+    function to display existing user
+    """
+    return User.display_user()
+
+def login_user(username,password):
+
+    """
+    function to check whether a user exist 
+    """
+    check_user = Details.verify_user(username,password)
+    return check_user
